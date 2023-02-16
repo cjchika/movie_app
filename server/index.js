a327e7bb58d07b1bd9f7e6ab2f6435e3bb974c31
Chijioke Chika <8pluscreative@gmail.com>
2023-02-16 06:26:31 +0100

    User

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
+import routes from "./routes/index.js";
 import "dotenv/config";
 
 const app = express();
@@ -11,6 +12,8 @@ app.use(cors());
 app.use(express());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use("/api/v1", routes);
 
 const port = process.env.PORT || 5000;
 
