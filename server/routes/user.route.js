7cc59057f44b7b43027d4ccb913007a44060ed0e
Chijioke Chika <8pluscreative@gmail.com>
2023-02-16 06:59:26 +0100

    Routes

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -110,6 +110,7 @@ router.post(
   body("mediaTitle").exists().withMessage("mediaTitle is required"),
   body("mediaPoster").exists().withMessage("mediaPoster is required"),
   body("mediaRating").exists().withMessage("mediaRating is required"),
+  requestHandler.validate,
   favoriteController.addFavorite
 );
 
