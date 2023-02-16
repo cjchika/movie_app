32f50763558946d9111f36c0321d637670d21ca2
Chijioke Chika <8pluscreative@gmail.com>
2023-02-16 05:49:51 +0100

    User

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -13,7 +13,7 @@ router.post(
   body("username")
     .exists()
     .withMessage("username is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("Username minimum 8 characters")
     .custom(async (value) => {
       const user = await userModel.findOne({ username: value });
@@ -22,12 +22,12 @@ router.post(
   body("password")
     .exists()
     .withMessage("password is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("Password minimum 8 characters"),
   body("confirmPassword")
     .exists()
     .withMessage("confirmPassword is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("Confirm password minimum 8 characters")
     .custom((value, { req }) => {
       if (value !== req.body.password)
@@ -37,7 +37,7 @@ router.post(
   body("displayName")
     .exists()
     .withMessage("displatName is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("displayName minimum 8 characters"),
   requestHandler.validate,
   userController.signup
@@ -48,12 +48,12 @@ router.post(
   body("username")
     .exists()
     .withMessage("username is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("username minimum 8 characters"),
   body("password")
     .exists()
     .withMessage("password is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("password minimum 8 characters"),
   requestHandler.validate,
   userController.signin
@@ -65,17 +65,17 @@ router.put(
   body("password")
     .exists()
     .withMessage("password is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("password minimum 8 characters"),
   body("newPassword")
     .exists()
     .withMessage("newPassword is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("newPassword minimum 8 characters"),
   body("confirmNewPassword")
     .exists()
     .withMessage("confirmNewPassword is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("confirmNewPassword minimum 8 characters")
     .custom((value, { req }) => {
       if (value !== req.body.password)
@@ -84,6 +84,39 @@ router.put(
     }),
   requestHandler.validate,
   userController.updatePassword
+);
+
+router.get("/info", tokenMiddleware.auth, userController.getInfo);
+
+router.get(
+  "/favorites",
+  tokenMiddleware.auth,
+  favoriteController.getFavoritesOfUser
+);
+
+router.post(
+  "/favorites",
+  tokenMiddleware.auth,
+  body("mediaType")
+    .exists()
+    .withMessage("mediaType is required")
+    .custom((type) => ["movie", "tv"].includes(type))
+    .withMessage("mediaType invalid"),
+  body("mediaId")
+    .exists()
+    .withMessage("mediaId is required")
+    .isLength({ min: 1 })
+    .withMessage("mediaId can not be empty"),
+  body("mediaTitle").exists().withMessage("mediaTitle is required"),
+  body("mediaPoster").exists().withMessage("mediaPoster is required"),
+  body("mediaRating").exists().withMessage("mediaRating is required"),
+  favoriteController.addFavorite
+);
+
+router.delete(
+  "/favorites/:favoriteId",
+  tokenMiddleware.auth,
+  favoriteController.removeFavorite
 );
 
 export default router;
