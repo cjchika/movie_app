6b13d09a2852af7ce768b68a35f0974dffae9437
Chijioke Chika <8pluscreative@gmail.com>
2023-02-25 07:48:48 +0100

    ThemeColor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
 import { ThemeProvider } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import themeConfigs from "./configs/theme.config";
+import { ToastContainer } from "react-toastify";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const App = () => {
+  const { themeMode } = useSelector((state) => state.themeMode);
+
   return (
-    <div className="App">
-      <h1>Bigger</h1>
-    </div>
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      {/* Config Toastify */}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      />
+      {/* MUI Reset CSS */}
+      <CssBaseline />
+    </ThemeProvider>
   );
 };
 
