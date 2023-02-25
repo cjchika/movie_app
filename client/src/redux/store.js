da890e8d0ed6786c1a2bfdd0e8e73d75a6e98ff2
Chijioke Chika <8pluscreative@gmail.com>
2023-02-25 06:04:42 +0100

    ThemeSlice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/userSlice";
+import themeModeSlice from "./features/themeModeSlice";
 
 const store = configureStore({
   reducer: {
-    userSlice,
+    user: userSlice,
+    themeMode: themeModeSlice,
   },
 });
 
