b1757352f7e694e5f3e22100dc63a9abec84b5f4
Chijioke Chika <8pluscreative@gmail.com>
2023-02-26 13:29:41 +0100

    Layout

---
+++ client/src/components/layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+
+const MainLayout = () => {
+  return (
+    <>
+      {/* Global Loading */}
+
+      {/* Login Modal */}
+    </>
+  );
+};
+
+export default MainLayout;
