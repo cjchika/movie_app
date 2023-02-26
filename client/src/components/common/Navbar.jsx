4f2b27ece00018c521b537ddd3736d0faedfe2f7
Chijioke Chika <8pluscreative@gmail.com>
2023-02-26 16:05:20 +0100

    Header

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -1,8 +1,47 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { cloneElement, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import MenuIcon from "@mui/icons-material/Menu";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  Toolbar,
+  useScrollTrigger,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import menuConfigs from "../../configs/menu.configs";
+import { themeMode } from "../../configs/theme.configs";
+import { setAuthModalOpen } from "../../redux/features/authModalSlice";
+import { setThemeMode } from "../../redux/features/themeModeSlice";
+import Logo from "./Logo";
 
 const ScrollAppBar = ({ children, window }) => {
   const { themeMode } = useSelector((state) => state.themeMode);
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 50,
+    target: window ? window() : undefined,
+  });
+
+  return cloneElement(children, {
+    sx: {
+      color: trigger
+        ? "text.primary"
+        : themeMode === themeMode.dark
+        ? "primary.contrastText"
+        : "text.primary",
+      backgroundColor: trigger
+        ? "background.paper"
+        : themeMode === themeMode.dark
+        ? "transparent"
+        : "background.paper",
+    },
+  });
 };
 
 const Navbar = () => {
