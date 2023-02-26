fae933ab25b3ca5d93474c264d7e9b6d4d96dee1
Chijioke Chika <8pluscreative@gmail.com>
2023-02-26 13:37:09 +0100

    Layout

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
@@ -6,6 +7,17 @@ const MainLayout = () => {
       {/* Global Loading */}
 
       {/* Login Modal */}
+
+      <Box display="flex" minHeight="100vh">
+        {/* Header */}
+
+        {/* Main */}
+        <Box component="main" flexGrow={1} overflow="hidden" minHeight="100vh">
+          <Outlet />
+        </Box>
+      </Box>
+
+      {/* Footer */}
     </>
   );
 };
