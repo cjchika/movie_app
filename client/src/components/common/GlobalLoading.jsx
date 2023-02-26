bcd497f1b229a52659f62050618e584d3c214757
Chijioke Chika <8pluscreative@gmail.com>
2023-02-26 15:05:15 +0100

    UI

---
+++ client/src/components/common/GlobalLoading.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Paper, Box, LinearProgress, Toolbar } from "@mui/material";
+import Logo from "./Logo";
+
+const GlobalLoading = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { globalLoading } = useSelector((state) => state.globalLoading);
+
+  useEffect(() => {
+    if (globalLoading) {
+      setIsLoading(true);
+    } else {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    }
+  }, [globalLoading]);
+
+  return (
+    <>
+      <Paper
+        sx={{
+          opacity: isLoading ? 1 : 0,
+          pointerEvents: "none",
+          transition: "all .3s ease",
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          zIndex: 999,
+        }}
+      >
+        <Toolbar />
+        <LinearProgress />
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Logo />
+        </Box>
+      </Paper>
+    </>
+  );
+};
+
+export default GlobalLoading;
--- client/src/redux/features/themeModeSlice.js
+++ client/src/redux/features/themeModeSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const themeModeSlice = createSlice({
   name: "ThemeMode",
   initialState: {
-    themeMode: "Dark",
+    themeMode: "dark",
   },
   reducers: {
     setThemeMode: (state, action) => {
