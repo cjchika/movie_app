23e7b6eea72cdbf30e3ae82dc148f80061d88a6c
Chijioke Chika <8pluscreative@gmail.com>
2023-03-01 06:36:25 +0100

    Sidebar

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import {
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import menuConfigs from "../../configs/menu.configs";
+import uiConfigs from "../../configs/ui.configs";
+import Logo from "./Logo";
+
+import { themeModes } from "../../configs/theme.configs";
+import { setThemeMode } from "../../redux/features/themeModeSlice";
 
 const Sidebar = ({ open, toggleSidebar }) => {
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.user);
+  const { appState } = useSelector((state) => state.appState);
+  const { themeMode } = useSelector((state) => state.themeMode);
+
+  const sidebarWidth = uiConfigs.size.sidebarWidth;
+
   return <div>Sidebar</div>;
 };
 
