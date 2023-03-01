ccee1626568615827b76bcef57a8f041c06379ba
Chijioke Chika <8pluscreative@gmail.com>
2023-03-01 13:35:14 +0100

    Sidebar

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -28,7 +28,118 @@ const Sidebar = ({ open, toggleSidebar }) => {
 
   const sidebarWidth = uiConfigs.size.sidebarWidth;
 
-  return <div>Sidebar</div>;
+  const onSwitchTheme = () => {
+    const theme =
+      themeMode === themeModes.dark ? themeModes.light : themeModes.dark;
+    dispatch(setThemeMode(theme));
+  };
+
+  const drawer = (
+    <>
+      <Toolbar sx={{ paddingY: "20px", color: "text.primary" }}>
+        <Stack width="100%" direction="row" justifyContent="center">
+          <Logo />
+        </Stack>
+      </Toolbar>
+      <List sx={{ paddingX: "30px" }}>
+        <Typography variant="h6" marginBottom="20px">
+          MENU
+        </Typography>
+        {menuConfigs.main.map((item, index) => (
+          <ListItemButton
+            key={index}
+            sx={{
+              borderRadius: "10px",
+              marginY: 1,
+              backgroundColor: appState.includes(item.state)
+                ? "primary.main"
+                : "unset",
+            }}
+            component={Link}
+            to={item.path}
+            onClick={() => toggleSidebar(false)}
+          >
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemText
+              disableTypography
+              primary={
+                <Typography textTransform="uppercase">
+                  {item.display}
+                </Typography>
+              }
+            />
+          </ListItemButton>
+        ))}
+
+        {user && (
+          <>
+            <Typography variant="h6" marginBottom="20px">
+              PERSONAL
+            </Typography>
+            {menuConfigs.user.map((item, index) => (
+              <ListItemButton
+                key={index}
+                sx={{
+                  borderRadius: "10px",
+                  marginY: 1,
+                  backgroundColor: appState.includes(item.state)
+                    ? "primary.main"
+                    : "unset",
+                }}
+                component={Link}
+                to={item.path}
+                onClick={() => toggleSidebar(false)}
+              >
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText
+                  disableTypography
+                  primary={
+                    <Typography textTransform="uppercase">
+                      {item.display}
+                    </Typography>
+                  }
+                />
+              </ListItemButton>
+            ))}
+          </>
+        )}
+
+        <Typography variant="h6" marginBottom="20px">
+          THEME
+        </Typography>
+        <ListItemButton onClick={onSwitchTheme}>
+          <ListItemIcon>
+            {themeMode === themeModes.dark && <DarkModeOutlinedIcon />}
+            {themeMode === themeModes.light && <WbSunnyOutlinedIcon />}
+          </ListItemIcon>
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography textTransform="uppercase">
+                {themeMode === themeModes.dark ? "dark mode" : "light mode"}
+              </Typography>
+            }
+          />
+        </ListItemButton>
+      </List>
+    </>
+  );
+
+  return (
+    <Drawer
+      open={open}
+      onClose={() => toggleSidebar(false)}
+      sx={{
+        "& .MuiDrawer-Paper": {
+          boxSizing: "border-box",
+          width: sidebarWidth,
+          borderRight: "0px",
+        },
+      }}
+    >
+      {drawer}
+    </Drawer>
+  );
 };
 
 export default Sidebar;
