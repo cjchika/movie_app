3cb7e49d63ddee8be24c92f1274eadbb0a920694
Chijioke Chika <8pluscreative@gmail.com>
2023-03-02 06:49:22 +0100

    Modal

---
+++ client/src/components/common/AuthModal.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { Box, Modal } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { setAuthModalOpen } from "../../redux/features/authModalSlice";
+import Logo from "./Logo";
+
+const actionState = {
+  signin: "signin",
+  signup: "signup",
+};
+
+const AuthModal = () => {
+  const { authModalOpen } = useSelector((state) => state.authModal);
+  const dispatch = useDispatch();
+  const [action, setAction] = useState(actionState.signin);
+
+  useEffect(() => {
+    if (authModalOpen) setAction(actionState.signin);
+  }, [authModalOpen]);
+
+  const handleClose = () => dispatch(setAuthModalOpen(false));
+
+  const switchAuthState = (state) => setAction(state);
+
+  return (
+    <Modal open={authModalOpen} onClose={handleClose}>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "100%",
+          maxWidth: "600%",
+          padding: 4,
+          outline: "none",
+        }}
+      >
+        <Box
+          sx={{
+            padding: 4,
+            boxShadow: 24,
+            backgroundColor: "background.paper",
+          }}
+        ></Box>
+      </Box>
+    </Modal>
+  );
+};
+
+export default AuthModal;
