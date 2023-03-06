24d973d4f353f16e82299c80175eb78fcd0757a8
Chijioke Chika <8pluscreative@gmail.com>
2023-03-06 06:21:19 +0100

    Hero Slide

---
+++ client/src/components/common/HeroSlide.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeroSlide = ({ mediaType, mediaCategory }) => {
+  return (
+    <>
+      <h1>HeroSlide</h1>
+      <h1>HeroSlide</h1>
+    </>
+  );
+};
+
+export default HeroSlide;
