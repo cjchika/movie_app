330cb428eb29f83b69b5e046cae57f161a4d76de
Chijioke Chika <8pluscreative@gmail.com>
2023-03-06 06:54:09 +0100

    Hero Slide

--- client/src/components/common/HeroSlide.jsx
+++ client/src/components/common/HeroSlide.jsx
@@ -1,6 +1,67 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import {
+  Box,
+  Button,
+  Chip,
+  Divider,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { AutoPlay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { toast } from "react-toastify";
+
+import { setGlobalLoading } from "../../redux/features/globalLoadingSlice";
+import { routesGen } from "../../routes/routes";
+
+import uiConfigs from "../../configs/ui.configs";
+
+import CircularRate from "./CircularRate";
+
+import tmdbConfigs from "../../api/configs/tmdb.configs";
+import genreApi from "../../api/modules/genre.api";
+import mediaApi from "../../api/modules/media.api";
 
 const HeroSlide = ({ mediaType, mediaCategory }) => {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+
+  const [movies, setMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    const getMedias = async () => {
+      const { response, err } = await mediaApi.getList({
+        mediaType,
+        mediaCategory,
+        page: 1,
+      });
+
+      if (response) setMovies(response.results);
+      if (err) toast.error(err.message);
+      dispatch(setGlobalLoading(false));
+    };
+    const getGenres = async () => {
+      dispatch(setGlobalLoading(true));
+      const { response, err } = await genreApi.getList({ mediaType });
+      if (response) {
+        setGenres(response.genres);
+        getMedias();
+      }
+
+      if (err) {
+        toast.error(err.message);
+        setGlobalLoading(false);
+      }
+    };
+
+    getGenres();
+  }, [mediaType, mediaCategory, dispatch]);
+
   return (
     <>
       <h1>HeroSlide</h1>
