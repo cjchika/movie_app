6b7d43972df6569af7079f23429fab905fa4e953
Chijioke Chika <8pluscreative@gmail.com>
2023-03-11 05:28:28 +0100

    Media item

--- client/src/components/common/MediaItem.jsx
+++ client/src/components/common/MediaItem.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import tmdbConfigs from "../../configs/ui.configs";
+import { routesGen } from "../../routes/routes";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import CircularRate from "./CircularRate";
+import { useSelector } from "react-redux";
+import favoriteUtils from "../../utils/favorite.utils";
 
 const MediaItem = ({ media, mediaType }) => {
+  const { listFavorites } = useSelector((state) => state.user);
   return <div>MediaItem</div>;
 };
 
