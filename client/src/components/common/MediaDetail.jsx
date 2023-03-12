457d7ade3f18bd5f9bd70a7bed3c4851de965bbf
Chijioke Chika <8pluscreative@gmail.com>
2023-03-12 03:25:56 +0100

    Media Details

--- client/src/components/common/MediaDetail.jsx
+++ client/src/components/common/MediaDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteeBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
@@ -18,7 +18,34 @@ import tmdbConfigs from "../../api/configs/tmdb.configs";
 import favoriteApi from "../../api/modules/favorite.api";
 import mediaApi from "../../api/modules/media.api";
 
+import { setGlobalLoading } from "../../redux/features/globalLoadingSlice";
+import { setAuthModalOpen } from "../../redux/features/authModalSlice";
+import { addFavorite, removeFavorite } from "../../redux/features/userSlice";
+
 const MediaDetail = () => {
+  const { mediaType, mediaId } = useParams();
+
+  const { user, listFavorites } = useSelector((state) => state.user);
+
+  const [media, setMedia] = useState();
+  const [isFavorite, setisFavorite] = useState(false);
+  const [onRequest, setOnRequest] = useState(false);
+  const [genres, setGenres] = useState([]);
+
+  const dispatch = useDispatch();
+
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const getMedia = async () => {
+      dispatch(setGlobalLoading(true));
+      const { response, err } = await mediaApi.getDetail({
+        mediaType,
+        mediaId,
+      });
+    };
+  }, [mediaType, mediaId, dispatch]);
+
   return <div>MediaDetail</div>;
 };
 
