815e6c66d5b87a94a562f91e3ed1c49e11652d84
Chijioke Chika <8pluscreative@gmail.com>
2023-03-13 05:34:42 +0100

    Favorite

--- client/src/pages/MediaDetail.jsx
+++ client/src/pages/MediaDetail.jsx
@@ -65,6 +65,7 @@ const MediaDetail = () => {
     if (onRequest) return;
 
     if (isFavorite) {
+      onRemoveFavorite();
       return;
     }
 
@@ -88,6 +89,28 @@ const MediaDetail = () => {
       dispatch(addFavorite(response));
       setIsFavorite(true);
       toast.success("Add favorite success");
+    }
+  };
+
+  const onRemoveFavorite = async () => {
+    if (onRequest) return;
+    setOnRequest(true);
+
+    const favorite = listFavorites.find(
+      (e) => e.mediaId.toString() === media.id.toString()
+    );
+
+    const { response, err } = await favoriteApi.remove({
+      favoriteId: favorite.id,
+    });
+
+    setOnRequest(false);
+
+    if (err) toast.error(err.message);
+    if (response) {
+      dispatch(removeFavorite(favorite));
+      setIsFavorite(false);
+      toast.success("Remove favorite success");
     }
   };
 
