ce01f042b9bab1f470c84ab2a738d28bb4aac3a1
Chijioke Chika <8pluscreative@gmail.com>
2023-03-14 06:29:17 +0100

    Review

--- client/src/components/common/MediaReview.jsx
+++ client/src/components/common/MediaReview.jsx
@@ -16,8 +16,49 @@ import { useSelector } from "react-redux";
 import Container from "./Container";
 import reviewApi from "../../api/modules/review.api";
 
-const MediaReview = () => {
-  return <div>MediaReview</div>;
+const MediaReview = ({ review, onRemoved }) => {
+  const { user } = useSelector((state) => state.user);
+  const [onRequest, setOnRequest] = useState(false);
+
+  const onRemove = async () => {
+    if (onRequest) return;
+    setOnRequest(true);
+
+    const { response, err } = await reviewApi.remove({ reviewId: review.id });
+
+    if (err) toast.error(err.message);
+    if (response) onRemoved(review.id);
+  };
+  return (
+    <Box
+      sx={{
+        padding: 2,
+        borderRadius: "5px",
+        position: "relative",
+        opacity: onRequest ? 0.6 : 1,
+        "&:hover": { backgroundColor: "background.paper" },
+      }}
+    >
+      <Stack direction="row" spacing={2}>
+        {/* Avatar */}
+
+        {/* Avatar */}
+        <Stack spacing={2} flexGrow={1}>
+          <Stack spacing={1}>
+            <Typography variant="h6" fontWeight="700">
+              {review.user.displayName}
+            </Typography>
+            <Typography variant="caption">
+              {dayjs(review.createdAt).format("DD-MM-YYYY HH:mm:ss")}
+            </Typography>
+          </Stack>
+          <Typography variant="body1" textAlign="justify">
+            {review.content}
+          </Typography>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 };
 
 export default MediaReview;
