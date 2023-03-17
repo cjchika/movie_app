e2314a5968cafe8c3a486aff1b8840ff77b38900
Chijioke Chika <8pluscreative@gmail.com>
2023-03-17 15:03:12 +0100

    Review

--- client/src/pages/ReviewList.jsx
+++ client/src/pages/ReviewList.jsx
@@ -1,4 +1,38 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { LoadingButton } from "@mui/lab";
+import { Box, Button, Divider, Stack, Typography } from "@mui/material";
+import dayjs from "dayjs";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
+import tmdbConfigs from "../api/modules/review.api";
+import reviewApi from "../api/modules/review.api";
+import Container from "../components/common/Container";
+import uiConfigs from "../configs/ui.configs";
+import { setGlobalLoading } from "../redux/features/globalLoadingSlice";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { routesGen } from "../routes/routes";
+
+const ReviewItem = ({ review, onRemoved }) => {
+  const dispatch = useDispatch();
+  const [onRequest, setOnRequest] = useState(false);
+
+  const onRemove = async () => {
+    if (onRequest) return;
+    setOnRequest(true);
+    const { response, err } = await reviewApi.remove({
+      favoriteId: media.id,
+    });
+    setOnRequest(false);
+
+    if (err) toast.error(err.message);
+    if (response) {
+      toast.success("Removed favorite successful.");
+      dispatch(removeFavorite({ mediaId: media.mediaId }));
+      onRemoved(media.id);
+    }
+  };
+};
 
 const ReviewList = () => {
   return <div>ReviewList</div>;
