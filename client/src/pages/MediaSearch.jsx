61aa8f8986b47bf3cd2bdff3068a66a9525090d6
Chijioke Chika <8pluscreative@gmail.com>
2023-03-17 12:06:25 +0100

    Media Search

--- client/src/pages/MediaSearch.jsx
+++ client/src/pages/MediaSearch.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import { useState, useEffect, useCallback } from "react";
+import { LoadingButton } from "@mui/lab";
+import { Box, Button, Stack, TextField, Toolbar } from "@mui/material";
+import { toast } from "react-toastify";
+import mediaApi from "../api/modules/media.api";
+import MediaGrid from "../components/common/MediaGrid";
+import uiConfigs from "../configs/ui.configs";
+
+const mediaTypes = ["movie", "tv", "people"];
+let timer;
+const timeout = 500;
 
 const MediaSearch = () => {
+  const [query, setQuery] = useState("");
+  const [onSearch, setOnSearch] = useState(false);
+  const [mediaType, setMediaType] = useState(mediaTypes[0]);
   return <div>MediaSearch</div>;
 };
 
