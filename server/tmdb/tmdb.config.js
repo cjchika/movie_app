56acb4eb0c4ccf41ff7731170b471b5e075f91fc
Chijioke Chika <8pluscreative@gmail.com>
2023-03-18 15:57:53 +0100

    Deployment

--- server/tmdb/tmdb.config.js
+++ server/tmdb/tmdb.config.js
@@ -1,5 +1,5 @@
-const baseUrl = process.env.TMDB_BASE_URL;
-const key = process.env.TMDB_KEY;
+const baseUrl = process.env.TMDB_BASE_URL || "https://api.themoviedb.org/3/";
+const key = process.env.TMDB_KEY || "972a02537ef524861a020eed189b31a7";
 
 const getUrl = (endpoint, params) => {
   const qs = new URLSearchParams(params);
