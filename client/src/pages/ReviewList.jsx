6ed0e5b78a6cbb27ec3c7489e335b52f3d71422c
Chijioke Chika <8pluscreative@gmail.com>
2023-03-18 05:54:18 +0100

    Review

--- client/src/pages/ReviewList.jsx
+++ client/src/pages/ReviewList.jsx
@@ -21,17 +21,87 @@ const ReviewItem = ({ review, onRemoved }) => {
     if (onRequest) return;
     setOnRequest(true);
     const { response, err } = await reviewApi.remove({
-      favoriteId: media.id,
+      reviewId: review.id,
     });
     setOnRequest(false);
 
     if (err) toast.error(err.message);
     if (response) {
-      toast.success("Removed favorite successful.");
-      dispatch(removeFavorite({ mediaId: media.mediaId }));
-      onRemoved(media.id);
+      toast.success("Removed review successful.");
+      onRemoved(review.id);
     }
   };
+
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        display: "flex",
+        flexDirection: {
+          xs: "column",
+          md: "row",
+          padding: 1,
+          opacity: onRequest ? 0.6 : 1,
+          "&:hover": { backgroundColor: "background.paper" },
+        },
+      }}
+    >
+      <Box sx={{ width: { xs: 0, md: "10%" } }}>
+        <Link
+          to={routesGen.mediaDetail(review.mediaType, review.mediaid)}
+          style={{ color: "unset", textDecoration: "none" }}
+        >
+          <Box
+            sx={{
+              paddingTop: "160%",
+              ...uiConfigs.style.backgroundImage(
+                tmdbConfigs.posterPath(review.mediaPoster)
+              ),
+            }}
+          />
+        </Link>
+      </Box>
+      <Box
+        sx={{
+          width: { xs: "100%", md: "80%" },
+          padding: { xs: 0, md: "0 2rem" },
+        }}
+      >
+        <Stack spacing={1}>
+          <Link
+            to={routesGen.mediaDetail(review.mediaType, review.mediaid)}
+            style={{ color: "unset", textDecoration: "none" }}
+          >
+            <Typography
+              variant="h6"
+              sx={{ ...uiConfigs.style.typoLines(1, "left") }}
+            >
+              {review.mediaTitle}
+            </Typography>
+          </Link>
+          <Typography variant="caption">
+            {dayjs(review.createdAt).format("DD-MM-YYYY HH:mm:ss")}
+          </Typography>
+          <Typography>{review.content}</Typography>
+        </Stack>
+      </Box>
+      <LoadingButton
+        variant="contained"
+        sx={{
+          position: { xs: "relative", md: "absolute" },
+          right: { xs: 0, md: "10px" },
+          marginTop: { xs: 2, md: 0 },
+          width: "max-content",
+        }}
+        startIcon={<DeleteIcon />}
+        loadingPosition="start"
+        loading={onRequest}
+        onClick={onRemove}
+      >
+        remove
+      </LoadingButton>
+    </Box>
+  );
 };
 
 const ReviewList = () => {
