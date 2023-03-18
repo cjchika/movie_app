2277f86ebbef4a6ff334d00486d4f41a1e78ed5e
Chijioke Chika <8pluscreative@gmail.com>
2023-03-18 06:39:28 +0100

    Password Update

--- client/src/pages/PasswordUpdate.jsx
+++ client/src/pages/PasswordUpdate.jsx
@@ -1,4 +1,15 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, TextField } from "@mui/material";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import Container from "../components/common/Container";
+import uiConfigs from "../configs/ui.configs";
+import userApi from "../api/modules/user.api";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setUser } from "../redux/features/userSlice";
+import { setAuthModalOpen } from "../redux/features/authModalSlice";
 
 const PasswordUpdate = () => {
   return <div>PasswordUpdate</div>;
